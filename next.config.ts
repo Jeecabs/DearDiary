88ec0a208df54aea2be5e0b59a860104edfc2728
Jeecabs <lachlan.jacobs@protonmail.com>
2024-12-05 15:43:50 +1100

    Update next.config.ts to ignore TypeScript build errors and disable ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Disable ESLint during builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
